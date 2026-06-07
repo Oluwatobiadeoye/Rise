638fe83f1dba0a61b495e96e76ec9df6296c3085
Oluwadara Adedeji <adedejioluwadara7@gmail.com>
2026-06-07 14:04:31 +0100

    team: added kunle pic

--- lib/team.ts
+++ lib/team.ts
@@ -10,6 +10,7 @@ import daraPhoto from "@/public/team/dara.png";
 import tobiPhoto from "@/public/team/tobi.jpeg";
 import quadriPhoto from "@/public/team/quadri.jpeg";
 import ayoPhoto from "@/public/team/ayo.png";
+import kunlePhoto from "@/public/team/kunle.png";
 
 export type TeamMember = {
   slug: string;
@@ -44,13 +45,21 @@ export const team: ReadonlyArray<TeamMember> = [
     slug: "kunle-oguntoye",
     name: "Kunle Oguntoye",
     role: "General Secretary and Director, Partnerships and Collaborations",
+    profession: "Civil Engineer",
+    location: "Iowa, United States of America",
+    photo: kunlePhoto,
+    bio: [
+      "Kunle is ",
+      "xxxxx",
+    ],
+    school: "EACOED Model High School, Isokun, Oyo, class of 2013.",
   },
   {
     slug: "ayo-salaudeen",
     name: "Ayo Salaudeen",
     role: "Chief Human Resource Manager",
-    profession: "AI Engineer",
-    location: "Dublin, Ireland",
+    profession: "Researcher",
+    location: "Helsinki, Finland",
     photo: ayoPhoto,
     bio: [
       "Ayo is a doctoral researcher in animal breeding and genetics with over three years of experience in research, data analysis, and scientific problem-solving. She currently works as a Research Scientist at the Natural Resources Institute Finland (Luke) while pursuing her PhD in Animal Breeding and Genomics at the University of Helsinki, Finland.",
@@ -59,15 +68,15 @@ export const team: ReadonlyArray<TeamMember> = [
     school: "SPED International Secondary School, Oyo, class of 2013.",
   },
   {
-    slug: "oluwadara-adedeji",
-    name: "Oluwadara Adedeji",
-    role: "Director, Media and Strategy",
-    profession: "AI Engineer",
-    location: "Dublin, Ireland",
-    photo: daraPhoto,
+    slug: "quadri-oseni",
+    name: "Abdul-Quadri Olayinka Oseni",
+    role: "Director, Community Engagement & Field Operations",
+    profession: "Surveyor",
+    location: "Lagos, Nigeria",
+    photo: quadriPhoto,
     bio: [
-      "Dara is an AI researcher and engineer specialising in digital mental health and human-centered AI, with over five years of experience. He is currently a PhD candidate at University College Dublin under the Research Ireland Centre for Research Training in Machine Learning (ML-Labs) programme. Having previously served as the Public Relations Officer of the Society of Electrical and Electronic Engineering Students (SEEES UI), he brings substantial experience in media communications and strategic engagement.",
-      "A recipient of multiple awards and scholarships, including the Mastercard Foundation Scholarship, he is committed to empowering young leaders to access global opportunities and drive positive change in their communities. He is passionate about community development, STEM education, and mental health.",
+      "Abdul-Quadri is a Surveyor and Geospatial Professional with over six years of experience in field operations, mapping, and community-focused projects. He has contributed to major projects, including the construction of Marina Interchange Hub developed in partnership with the Agence Française de Développement (AFD), and has actively supported youth development through the Young Surveyors Network (YSN) Lagos state by organizing training, mentorship, and capacity-building programs.",
+      "Having held several leadership positions, including Chairman of the O3 Spell Quest Competition and Convention Chairman at the Junior Chamber International Federal School of Surveying , Oyo,  he has developed strong skills in team coordination, stakeholder engagement, and project execution. He believes in using knowledge, leadership, and service to inspire others to pursue excellence, embrace continuous learning, and contribute meaningfully to society. Through his work and volunteer engagements, he hopes to empower young people with the skills, confidence, and opportunities needed to create positive change in their communities and beyond.",
     ],
     school: "SPED International Secondary School, Oyo, class of 2013.",
   },
@@ -85,15 +94,15 @@ export const team: ReadonlyArray<TeamMember> = [
     school: "Nesto College, Erelu, Oyo, class of 2013.",
   },
   {
-    slug: "quadri-oseni",
-    name: "Abdul-Quadri Olayinka Oseni",
-    role: "Director, Community Engagement & Field Operations",
-    profession: "Surveyor",
-    location: "Lagos, Nigeria",
-    photo: quadriPhoto,
+    slug: "oluwadara-adedeji",
+    name: "Oluwadara Adedeji",
+    role: "Director, Media and Strategy",
+    profession: "AI Engineer",
+    location: "Dublin, Ireland",
+    photo: daraPhoto,
     bio: [
-      "Abdul-Quadri is a Surveyor and Geospatial Professional with over six years of experience in field operations, mapping, and community-focused projects. He has contributed to major projects, including the construction of Marina Interchange Hub developed in partnership with the Agence Française de Développement (AFD), and has actively supported youth development through the Young Surveyors Network (YSN) Lagos state by organizing training, mentorship, and capacity-building programs.",
-      "Having held several leadership positions, including Chairman of the O3 Spell Quest Competition and Convention Chairman at the Junior Chamber International Federal School of Surveying , Oyo,  he has developed strong skills in team coordination, stakeholder engagement, and project execution. He believes in using knowledge, leadership, and service to inspire others to pursue excellence, embrace continuous learning, and contribute meaningfully to society. Through his work and volunteer engagements, he hopes to empower young people with the skills, confidence, and opportunities needed to create positive change in their communities and beyond.",
+      "Dara is an AI researcher and engineer specialising in digital mental health and human-centered AI, with over five years of experience. He is currently a PhD candidate at University College Dublin under the Research Ireland Centre for Research Training in Machine Learning (ML-Labs) programme. Having previously served as the Public Relations Officer of the Society of Electrical and Electronic Engineering Students (SEEES UI), he brings substantial experience in media communications and strategic engagement.",
+      "A recipient of multiple awards and scholarships, including the Mastercard Foundation Scholarship, he is committed to empowering young leaders to access global opportunities and drive positive change in their communities. He is passionate about community development, STEM education, and mental health.",
     ],
     school: "SPED International Secondary School, Oyo, class of 2013.",
   },
